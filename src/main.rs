6c54067e71248a1e79b149639b6bc57e4d2e8fc2
asd988 <81303551+asd988@users.noreply.github.com>
2024-04-22 21:28:23 +0200

    pest test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct MyParser;
+
 fn main() {
-    println!("Hello, world!");
+    dbg!(MyParser::parse(Rule::init, "abc"));
 }
