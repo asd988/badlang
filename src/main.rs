65093cf12d567b48cb008d6b09f07f69514e738f
asd988 <no@emailfor.you>
2024-04-23 13:25:57 +0200

    create grammar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use pest::Parser;
 use pest_derive::Parser;
 
@@ -5,6 +7,29 @@ use pest_derive::Parser;
 #[grammar = "grammar.pest"]
 pub struct MyParser;
 
+struct Program {
+    instructions: Vec<Instruction>,
+    variables: HashMap<String, i64>,
+    tags: HashMap<String, Tag>,
+    call_stack: Vec<usize>,
+    next_instruction: usize,
+}
+
+enum Tag {
+    Normal(usize),
+    Stacked(usize)
+}
+
+enum Instruction {
+    Assignment,
+    Print,
+}
+
 fn main() {
-    dbg!(MyParser::parse(Rule::init, "abc"));
+    let result = MyParser::parse(Rule::init, "a = -10\na +s= 15\n< a").unwrap();
+
+    for pair in result {
+        println!("p {:?}", pair);
+    }
+
 }
