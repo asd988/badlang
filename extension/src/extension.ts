11cb2ab86d063292e69c88783599583d0c6e38da
asd988 <no@emailfor.you>
2024-04-26 15:32:51 +0200

    lsp finally connected

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,23 +1,56 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {
+	ServerOptions,
+	LanguageClientOptions,
+	LanguageClient
+} from "vscode-languageclient/node";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let client: LanguageClient;
+
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "badlang" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('badlang.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showInformationMessage('Hello World!!');
 	});
 
 	context.subscriptions.push(disposable);
+
+    // This line of code will only be executed once when your extension is activated
+
+    // TODO: Start server exe and communicate with it
+    const serverExe = process.env.SERVER_PATH || "lsp";
+
+	const exe = {command: serverExe, args:['-lsp'], options: {env: {RUST_LOG: "debug"}}};
+    const ServerOptions: ServerOptions = {
+        run: exe,
+        debug: exe
+    };
+
+    const clientOptions: LanguageClientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [
+            {
+                pattern: '**/*.txt',
+            }
+        ],
+
+    };
+
+    client = new LanguageClient("Hello LSP", ServerOptions, clientOptions);
+
+    // For debugging only
+    //lspClient.trace = Trace.Verbose;
+
+    //add all disposables here
+    client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
 }
